7b90ef2d0bbbb88e3b92ccb02cf21fec3c4c53a1
Yonghoon JunG <asdf2017@naver.com>
2021-10-19 16:27:45 +0900

    update ffmpeg

---
+++ keyFrameExtract.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#!/bin/bash
+# extract frames from multiple videos
+# google search key word : How to extract frames from all videos in a folder using ffmpeg
+# code reference : https://stackoverflow.com/questions/13408493/an-and-operator-for-an-if-statement-in-bash
+
+# this code must be modify if added usable extension
+if [ "$1" == '' && "$1" != 'MP4' && "$2" != 'MOV' ]; then
+    echo "Usage: $0 <FILE-EXTENSIONS, MP4|MOV|ETC>";
+    exit;
+fi
+
+fileext = $1;
+
+resultfolder="frames";
+[ ! -d "$resultfolder" ] && mkdir "$resultfolder";
+
+# loop for frame extraction from videos
+for file in ./*."$fileext"; do
+    result = "$resultfolder/${file}"
+    mkdir -p "$result"
+    ffmpeg -i "$file" -q:v 2 -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 0 "$result/frame%03d.jpg";
+done
